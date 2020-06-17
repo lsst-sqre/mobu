33b10005a916c10e7bbc2014d8b2cd5286c86022
Christine Banek <cbanek@gmail.com>
2020-06-16 21:08:09 -0700

    [DM-25328] Add more logging for login loop
    More logging right before and after will help since each line has
    a timestamp.

--- src/sciencemonkey/business.py
+++ src/sciencemonkey/business.py
@@ -28,27 +28,42 @@ class Business:
             await asyncio.sleep(5)
 
     def dump(self) -> dict:
-        return {"business": "Idle"}
+        return {"name": "Idle"}
 
 
 @dataclass
 class JupyterLoginLoop(Business):
+    success_count: int = 0
+    failure_count: int = 0
+
     async def run(self) -> None:
-        logger = self.monkey.log
-        logger.info("Starting up...")
+        try:
+            logger = self.monkey.log
+            logger.info("Starting up...")
 
-        client = JupyterClient(self.monkey.user, logger)
-        await client.hub_login()
+            client = JupyterClient(self.monkey.user, logger)
+            await client.hub_login()
+            logger.info("Logged into hub")
 
-        while True:
-            await client.ensure_lab()
-            await asyncio.sleep(60)
-            await client.delete_lab()
-            await asyncio.sleep(60)
-            logger.info("Next iteration")
+            while True:
+                logger.info("Starting next iteration")
+                await client.ensure_lab()
+                logger.info("Lab created.")
+                await asyncio.sleep(60)
+                logger.info("Deleting lab.")
+                await client.delete_lab()
+                self.success_count += 1
+                logger.info("Lab successfully deleted.")
+                await asyncio.sleep(60)
+        finally:
+            self.failure_count += 1
 
     def dump(self) -> dict:
-        return {"business": "JupyterLoginLoop"}
+        return {
+            "name": "JupyterLoginLoop",
+            "failure_count": self.failure_count,
+            "success_count": self.success_count,
+        }
 
 
 @dataclass
@@ -69,4 +84,4 @@ class JupyterPythonLoop(Business):
             await asyncio.sleep(60)
 
     def dump(self) -> dict:
-        return {"business": "JupyterPythonLoop"}
+        return {"name": "JupyterPythonLoop"}
