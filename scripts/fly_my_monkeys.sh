c26b375b2240799fafbcac61dd2d03ac640bbaa1
Christine Banek <cbanek@gmail.com>
2020-06-25 12:44:41 -0700

    [DM-25653] Update fly script
    Add in the ability to pass in an environment and use an access
    token from the environment.  Useful for spawning on int and stable.

--- scripts/fly_my_monkeys.sh
+++ scripts/fly_my_monkeys.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -xe
 
-curl -X POST -d "@lsptestuser01.json" -H "Content-Type: application/json" http://localhost:8000/mobu/user | python -m json.tool
-curl -X POST -d "@lsptestuser02.json" -H "Content-Type: application/json" http://localhost:8000/mobu/user | python -m json.tool
-curl -X POST -d "@lsptestuser03.json" -H "Content-Type: application/json" http://localhost:8000/mobu/user | python -m json.tool
-curl -X POST -d "@lsptestuser04.json" -H "Content-Type: application/json" http://localhost:8000/mobu/user | python -m json.tool
-curl -X POST -d "@lsptestuser05.json" -H "Content-Type: application/json" http://localhost:8000/mobu/user | python -m json.tool
+ENVIRONMENT=${1:-"http://localhost:8000"}
+
+curl -X POST -d "@lsptestuser01.json" -H "Content-Type: application/json" -u $ACCESS_TOKEN: $ENVIRONMENT/mobu/user | python -m json.tool
+curl -X POST -d "@lsptestuser02.json" -H "Content-Type: application/json" -u $ACCESS_TOKEN: $ENVIRONMENT/mobu/user | python -m json.tool
+curl -X POST -d "@lsptestuser03.json" -H "Content-Type: application/json" -u $ACCESS_TOKEN: $ENVIRONMENT/mobu/user | python -m json.tool
+curl -X POST -d "@lsptestuser04.json" -H "Content-Type: application/json" -u $ACCESS_TOKEN: $ENVIRONMENT/mobu/user | python -m json.tool
+curl -X POST -d "@lsptestuser05.json" -H "Content-Type: application/json" -u $ACCESS_TOKEN: $ENVIRONMENT/mobu/user | python -m json.tool
