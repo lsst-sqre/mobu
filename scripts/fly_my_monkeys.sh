651250ae4a4b237c6c01009cbd5c37ae96f09ce4
Christine Banek <cbanek@gmail.com>
2021-02-17 18:37:04 -0700

    [DM-28840] Allow jupyter spawner options to be configured
    Make it so that each monkey knows what data it should use for making
    a lab.  This is useful when we want monkeys to use different images,
    or different sizes, or a mix off them.

--- scripts/fly_my_monkeys.sh
+++ scripts/fly_my_monkeys.sh
@@ -5,5 +5,5 @@ MONKEY_DIR=${2:-"nublado"}
 cd $2
 for MONKEY in *
 do
-  curl -X POST -d "@$MONKEY" -H "Content-Type: application/json" -u $ACCESS_TOKEN: $ENVIRONMENT /mobu/user | python -m json.tool
+  curl -X POST -d "@$MONKEY" -H "Content-Type: application/json" -u $ACCESS_TOKEN: $ENVIRONMENT/mobu/user | python -m json.tool
 done
