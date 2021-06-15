cb5cd170429ae5c6a66d751123f83a364c4c4e30
adam <athornton@lsst.org>
2021-06-14 17:08:41 -0700

    Shut down monkeys on DELETE

--- src/mobu/jupyterpythonloop.py
+++ src/mobu/jupyterpythonloop.py
@@ -8,7 +8,7 @@ __all__ = [
 ]
 
 import asyncio
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from mobu.business import Business
 from mobu.jupyterclient import JupyterClient
@@ -16,11 +16,14 @@ from mobu.jupyterclient import JupyterClient
 
 @dataclass
 class JupyterPythonLoop(Business):
+    _client: JupyterClient = field(init=False)
+
     async def run(self) -> None:
         logger = self.monkey.log
         logger.info("Starting up...")
 
         client = JupyterClient(self.monkey.user, logger, self.options)
+        self._client = client
         await client.hub_login()
         await client.ensure_lab()
 
@@ -30,6 +33,9 @@ class JupyterPythonLoop(Business):
             reply = await client.run_python(kernel, "print(2+2)")
             logger.info(reply)
             await asyncio.sleep(60)
+
+    async def stop(self) -> None:
+        await self._client.delete_lab()
 
     def dump(self) -> dict:
         return {"name": "JupyterPythonLoop"}
