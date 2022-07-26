4b100de9e8842c1a571553b276bf7a88cf152afd
Christine Banek <cbanek@gmail.com>
2022-07-26 09:34:42 -0700

    [DM-35717] Fix query typo
    Extra space in username is no good.

--- src/mobu/templates/tapqueryrunner/dp0.1/object-photometry.sql
+++ src/mobu/templates/tapqueryrunner/dp0.1/object-photometry.sql
@@ -1,4 +1,4 @@
-SELECT '{{ user name }}', 'object-photometry', *
+SELECT '{{ username }}', 'object-photometry', *
 FROM dp01_dc2_catalogs.object AS o
 JOIN dp01_dc2_catalogs.forced_photometry AS fp
 ON o.objectId = fp.objectId
