f957c59f2869f5d541c866b9466aa0d47455333e
Russ Allbery <rra@lsst.org>
2023-12-15 13:11:35 -0800

    Simplify the Docker build
    Since we no longer upload intermediate stages, the separation
    between the dependencies stage and the install stage became
    irrelevant and they can be combined. Move the installation of Git
    from the base image to the dependency image, since it's only
    required during installation.

--- scripts/install-dependency-packages.sh
+++ scripts/install-dependency-packages.sh
@@ -10,25 +10,26 @@
 
 # Bash "strict mode", to help catch problems and bugs in the shell
 # script. Every bash script you write should include this. See
-# http://redsymbol.net/articles/unofficial-bash-strict-mode/ for
-# details.
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/ for details.
 set -euo pipefail
 
 # Display each command as it's run.
 set -x
 
-# Tell apt-get we're never going to be able to give manual
-# feedback:
+# Tell apt-get we're never going to be able to give manual feedback.
 export DEBIAN_FRONTEND=noninteractive
 
-# Update the package listing, so we know what packages exist:
+# Update the package listing, so we know what packages exist.
 apt-get update
 
-# Install build-essential because sometimes Python dependencies need to build
-# C modules, particularly when upgrading to newer Python versions.  libffi-dev
-# is sometimes needed to build cffi (a cryptography dependency).
-apt-get -y install --no-install-recommends build-essential libffi-dev
+# Install various dependencies that may be required to install JupyterHub or
+# our add-on modules, or are wanted at runtime:
+#
+# build-essential: sometimes needed to build Python modules
+# git: required by setuptools_scm
+# libffi-dev: sometimes needed to build cffi, a cryptography dependency
+apt-get -y install --no-install-recommends build-essential git libffi-dev
 
-# Delete cached files we don't need anymore:
+# Delete cached files we don't need anymore.
 apt-get clean
 rm -rf /var/lib/apt/lists/*
