948a39aa9ac32dc12e3abdbdc33bb5ba5056f461
Russ Allbery <rra@lsst.org>
2025-12-16 14:51:42 -0800

    Update shared files from template
    Update `ruff-shared.toml` and the `update-uv-version.sh` script,
    which have minor changes.

--- scripts/update-uv-version.sh
+++ scripts/update-uv-version.sh
@@ -7,17 +7,18 @@
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/ for details.
 set -euo pipefail
 
-# Determine the current frozen uv version. Since the package depends on
-# tox-uv, uv should always be part of the tox group dependencies.
-uv_version=$(uv export -q --no-hashes --only-group tox \
+# Determine the current frozen uv version. Since the lint group depends on
+# pre-commit-uv, uv should always be part of its dependencies and thus updated
+# when running uv lock --upgrade, which should be run before this script.
+uv_version=$(uv export -q --no-hashes --only-group lint \
              | grep ^uv== | sed 's/.*=//')
 
 # Replace the version in the env variables in GitHub Actions workflows.
 for f in .github/workflows/*.yaml; do
     sed "s/UV_VERSION: .*/UV_VERSION: \"$uv_version\"/" "$f" >"$f.n"
     if ! cmp -s "$f" "${f}.n"; then
-	echo "Updating UV_VERSION to $uv_version in $f"
-	mv "${f}.n" "$f"
+        echo "Updating UV_VERSION to $uv_version in $f"
+        mv "${f}.n" "$f"
     else
         rm "${f}.n"
     fi
@@ -28,7 +29,7 @@ for f in Dockerfile*; do
     sed "s/uv:[0-9][0-9.]*/uv:$uv_version/" "$f" >"${f}.n"
     if ! cmp -s "$f" "${f}.n"; then
         echo "Updating uv container version to $uv_version in $f"
-	mv "${f}.n" "$f"
+        mv "${f}.n" "$f"
     else
         rm "${f}.n"
     fi
